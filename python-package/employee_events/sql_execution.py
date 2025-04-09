a4079da75271f2496a8f262ca20b925c4cd7e8ff
Carolina-Barros <carito.barros86@gmail.com>
2025-04-09 19:52:51 +0200

    feat: progress in the query classes

--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 # Using pathlib, create a `db_path` variable
 # that points to the absolute path for the `employee_events.db` file
-#### YOUR CODE HERE
-
+# Use the current working directory as the base
+db_path = (Path.cwd() / 'employee_events.db').resolve()
 
 # OPTION 1: MIXIN
 # Define a class called `QueryMixin`
@@ -16,18 +16,24 @@ class QueryMixin:
     # that receives an sql query as a string
     # and returns the query's result
     # as a pandas dataframe
-    #### YOUR CODE HERE
+    def pandas_query(self, sql_query:str):
+        with connect (self.db_path) as conn:
+            return pd.read_sql_query(sql_query,conn)
 
     # Define a method named `query`
     # that receives an sql_query as a string
     # and returns the query's result as
     # a list of tuples. (You will need
     # to use an sqlite3 cursor)
-    #### YOUR CODE HERE
+    def query(self, sql_query:str):
+        with connect(self.db_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql_query)
+            return cursor.fetchall()
     
 
  
- # Leave this code unchanged
+# Leave this code unchanged
 def query(func):
     """
     Decorator that runs a standard sql execution
